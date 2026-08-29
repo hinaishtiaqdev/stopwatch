02cb5f2f338a6c4dbdcae5c55b87644bcf5ca0d5
Hina Ishtiaq <hinaishtiaqdev@gmail.com>
2026-08-29 09:12:54 +0500

    Delete src directory

--- src/redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-
-import stopwatchReducer from "./stopwatchSlice";
-
-export const store = configureStore({
-  reducer: {
-    stopwatch: stopwatchReducer
-  }
-});
